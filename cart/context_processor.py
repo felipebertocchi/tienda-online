a3e4f2853bf592b04b1a9cde9e8edd8db62050ba
Felipe Bertocchi <fabertocchi@gmail.com>
2021-12-14 23:58:31 -0300

    Bug fix in context_processor

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -3,12 +3,14 @@
 def total_amount_cart(request):
     total = 0
     # if request.user.is_authenticated:
-    for value in request.session["cart"].values():
-        total += float(value["price"])
+    if request.session.get("cart"):
+        for value in request.session["cart"].values():
+            total += float(value["price"])
     return {"total_amount_cart":round(total, 2)}
 
 def total_items_cart(request):
     num_items = 0
-    for value in request.session["cart"].values():
-        num_items += value["quantity"]
+    if request.session.get("cart"):
+        for value in request.session["cart"].values():
+            num_items += value["quantity"]
     return {"total_items_cart":num_items}
